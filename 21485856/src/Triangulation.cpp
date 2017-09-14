27eb407978aafdfe13c3b6b42c812b8c2d70a6a9
sophie1992 <30907609+sophie1992@users.noreply.github.com>
2017-09-14 16:20:29 +0200

    Update comments in Triangulation

--- 21485856/src/Triangulation.cpp
+++ 21485856/src/Triangulation.cpp
@@ -11,6 +11,8 @@ Triangulation::Triangulation(const Triangulation &other) : calib(other.calib) {
 }
 
 vector<Point3f> Triangulation::operator()(const vector<Point2f> &markers1, const vector<Point2f> &markers2) const {
+	//triangulate the positions for a single frame
+	
 	//throw "Triangulation::operator() is not implemented";
 	vector<Point3f> resultofFrame;
 	//resultofFrame.resize(2);
@@ -18,13 +20,19 @@ vector<Point3f> Triangulation::operator()(const vector<Point2f> &markers1, const
 	Mat extrinCamera1 = (Mat_<float>(3, 4) << 1,0,0,0,0,1,0,0,0,0,1,0);
 	Mat projMatCamera1 = Mat_<float>(3, 4);
 	Mat projMatCamera2 = Mat_<float>(3, 4);
+	
+	//calculate projection matrix of camere
 	projMatCamera1 = calib.getCamera1()*extrinCamera1;
 	projMatCamera2 = calib.getCamera2()*calib.getTransCamera1Camera2();
 
 	Mat fundMat = calib.getFundamentalMat();
+	//projecting 3D points into the image plane using a perspective transformation
+	//http://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
 	correctMatches(fundMat, markers1, markers2,markers1, markers2);
+	//Reconstructs points by triangulation
 	triangulatePoints(projMatCamera1, projMatCamera2, markers1, markers2, pnts3D);
-
+	
+	//prepare Input vector of N-dimensional points for function convertPointsFromHomogeneous
 	Mat expandTrans;
 	calib.getTransCamera1World().copyTo(expandTrans);
 	Mat elmemt4expand = (Mat_<float>(1, 4) << 0, 0, 0, 1);
@@ -32,6 +40,7 @@ vector<Point3f> Triangulation::operator()(const vector<Point2f> &markers1, const
 	//cerr << expandTrans << endl;
 
 	Mat result = expandTrans * pnts3D;
+	//Converts points from homogeneous to Euclidean space
 	convertPointsFromHomogeneous(result.t(), resultofFrame);
 
 	//resultofFrame.insert(); //result.col(0);
@@ -40,6 +49,8 @@ vector<Point3f> Triangulation::operator()(const vector<Point2f> &markers1, const
 }
 
 vector<vector<Point3f>> Triangulation::operator()(const vector<vector<Point2f>> &markers1, const vector<vector<Point2f>> &markers2) const {
+	//triangulate the positions for a whole sequence
+	
 	// do nothing if there is no data
 	if (markers1.empty()) {
 		return vector<vector<Point3f>>();
@@ -62,7 +73,10 @@ vector<vector<Point3f>> Triangulation::operator()(const vector<vector<Point2f>> 
 	return result;
 }
 
+
 vector<vector<Point3f>> Triangulation::calculateMotion(const vector<vector<Point3f>> &data) {
+	//get the triangulated marker data and will calculate the motion from it. 
+	//The motion is nothing but the marker position relative to the first marker in the first frame.
 	//throw "Triangulation::calculateMotion is not implemented";
 
 	vector<vector<Point3f>> output;
